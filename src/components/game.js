13a1542b3e8c371bcf0b5c397277bd8c8c0324b1
James Stuckey <jstuckey@rutgers.edu>
2018-06-21 17:38:22 -0400

    working on compareNumbers

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,49 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            guesses: [],
+            feedback: 'Take a guess!',
+            // secretNum: Math.round(Math.random()*100)
+            secretNum: 4
+        }
+
+        this.submit = this.submit.bind(this);
+    }
+
+    submit(e){
+        e.preventDefault()
+        const guess = (e.target.userGuess.value);
+
+        if (!this.state.guesses.includes(guess)) {
+            this.compareNumbers(guess);
+            this.setState({
+                guesses: this.state.guesses.concat(guess)
+            })
+        } else {
+            alert('You\'ve already guessed that number!');
+        }
+    }
+
+    compareNumbers(guess){
+        if (guess < this.state.secretNum) {
+            this.setState({ ...this.state, feedback: 'cold' });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.feedback} submit={this.submit}/>
+                <GuessCount count={this.state.guesses.length} />
+                <GuessList guesses={this.state.guesses} />
+            </div>
+        );
+    };
 }
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
+
     return (
-        <form>
+        <form onSubmit={e => props.submit(e)}>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
                 placeholder="Enter your Guess" required />
